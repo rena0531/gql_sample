f7da3732020ef4c97aa34109c55e3aee96e11042
Rena <choco.rena0609@gmail.com>
2020-04-27 18:21:39 +0900

    tutorial

---
+++ graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+import (
+	"github.com/rena/gql_sample/graph/model"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
